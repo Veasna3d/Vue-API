c1188c7c1401636cacfcf6cca8f8cfa228ea90e6
Veasna Pen <Veasna3d@gmail.com>
2023-09-02 22:40:02 +0700

    Init Project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About Us Page</h1>
+  </div>
+</template>
+
---
+++ src/views/Students/View.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="about">
+      <h1>Student Page</h1>
+    </div>
+  </template>
+  
+  
